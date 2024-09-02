2586cc5ebf465532a3e0935bc70861535eea7137
“Nahuel <nnahuell.gb@hotmail.com>
2024-09-02 15:43:15 -0300

    add ng for and style burger menu

--- src/app/core/main/feature/feature.component.ts
+++ src/app/core/main/feature/feature.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-feature',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './feature.component.html',
   styleUrl: './feature.component.scss'
 })
 export class FeatureComponent {
-
+  features = [
+    {
+      icon: 'add_box',
+      title: 'Easy to Use',
+      description: 'Editing and customizing Essential Landing is easy and fast'
+    },
+    {
+      icon: 'star_half',
+      title: '100% Responsive',
+      description: 'Editing and customizing Essential Landing is easy and fast'
+    },
+    {
+      icon: 'system_update_alt',
+      title: '50+ New pages',
+      description: 'Editing and customizing Essential Landing is easy and fast'
+    }
+  ];
 }
--- src/app/core/main/presentacion/img-tec/img-tec.component.ts
+++ src/app/core/main/presentacion/img-tec/img-tec.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-img-tec',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './img-tec.component.html',
   styleUrl: './img-tec.component.scss'
 })
 export class ImgTecComponent {
-
+// Datos de las imágenes
+images = [
+  {
+    class: 'img-tec-1',
+    src: 'https://technologywithpurpose.org/wp-content/uploads/2022/10/technology-with-purpose.svg',
+    alt: 'technology-withPurpose'
+  },
+  {
+    class: 'img-tec-2',
+    src: 'https://technologywithpurpose.org/wp-content/uploads/2023/02/xacademy-white.svg',
+    alt: 'xAcademy'
+  }
+];
 }
